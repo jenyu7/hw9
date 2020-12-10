c9986a72fe1e450602146f0b667d4d6b011d2178
sammyshaker <sshaker@caltech.edu>
2020-12-09 16:47:56 -0800

    Update __init__.py

--- hw9/__init__.py
+++ hw9/__init__.py
@@ -1,6 +1,8 @@
-from hw9 import *
+from .analysis import *
+from .mle import *
+from .reps import *
 
 __author__ = 'Jen Yu, Sammy Shaker, Arun Chakravorty'
 __email__ = 'jenyu@caltech.edu'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
